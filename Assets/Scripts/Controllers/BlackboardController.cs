5e6d41d067188bbd072aa92c3331bf452613c805
Bartek Stopa <stopa323@gmail.com>
2020-09-27 21:54:29 +0300

    Spawn resource element on LMB click on the board

--- Assets/Scripts/Controllers/BlackboardController.cs
+++ Assets/Scripts/Controllers/BlackboardController.cs
@@ -1,15 +1,34 @@
 ﻿using UnityEngine;
+using UnityEngine.EventSystems;
 
 namespace h8s
 {
-    public class BlackboardController : MonoBehaviour
+    public class BlackboardController : MonoBehaviour, IPointerClickHandler
     {
         public static BlackboardController Instance { get; private set; }
+        public static Canvas GUICanvas { get; private set; }
+        public static RectTransform GUICanvasRect { get; private set; }
 
         private void Awake()
         {
             if (Instance != null && Instance != this) { Destroy(gameObject); }
             else { Instance = this; }
+
+            /** Cache reference to major components **/
+            GUICanvas = GetComponent<Canvas>();
+            GUICanvasRect = GetComponent<RectTransform>();
+        }
+
+        public void OnPointerClick(PointerEventData eventData)
+        {
+            switch(eventData.button)
+            {
+                case PointerEventData.InputButton.Left:
+                    ResourceController.Instance.CreateResource(eventData.position);
+                    break;
+                default:
+                    break;
+            }
         }
     }
 }
