258535d1e9f32bac1f5af6d12616d0036872f163
stopa323 <stopa323@gmail.com>
2020-09-27 22:04:00 +0300

    Add blackboard & resource spawn function (#2)
    * Add blackboard bg image with grid
    
    * Add initial node element prefab
    
    * Spawn resource element on LMB click on the board

---
+++ Assets/Scripts/Controllers/ResourceController.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+
+namespace h8s
+{
+    public class ResourceController : MonoBehaviour
+    {
+        public static ResourceController Instance { get; private set; }
+
+        [SerializeField] private GameObject resourcePrefab;
+
+        private void Awake()
+        {
+            if (Instance != null && Instance != this) { Destroy(gameObject); }
+            else { Instance = this; }
+        }
+
+        public void CreateResource(Vector2 position)
+        {
+            Debug.Log(string.Format("Creating resource at {0}", position));
+
+            // Spawn resource object attached to node container
+            var node_obj = Instantiate(resourcePrefab, transform) as GameObject;
+
+            // Move node to requested position
+            var spawn_position = Utility.CanvasPositionFromScreen(position);
+            Debug.Log(spawn_position);
+            node_obj.transform.localPosition = new Vector3(spawn_position.x, spawn_position.y);
+        }
+    }
+}
---
+++ Assets/Scripts/Utility.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+namespace h8s
+{
+    public static class Utility
+    {
+        public static Vector2 CanvasPositionFromScreen(Vector2 screenPosition)
+        {
+            var transformed_position = new Vector2(
+                screenPosition.x, 
+                screenPosition.y - BlackboardController.GUICanvasRect.rect.height);
+
+            return transformed_position;
+        }
+    }
+
+}
