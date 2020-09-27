db9cf21c9b907a3af02447a1f4d670652631379d
Bartek Stopa <stopa323@gmail.com>
2020-09-27 18:51:57 +0300

    Add initial node element prefab

---
+++ Assets/Scripts/Controllers/BlackboardController.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+namespace h8s
+{
+    public class BlackboardController : MonoBehaviour
+    {
+        public static BlackboardController Instance { get; private set; }
+
+        private void Awake()
+        {
+            if (Instance != null && Instance != this) { Destroy(gameObject); }
+            else { Instance = this; }
+        }
+    }
+}
