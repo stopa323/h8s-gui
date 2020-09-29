e9f15879c0883ceb96d0084d4165f2019c09d7da
Bartek Stopa <stopa323@gmail.com>
2020-09-29 17:39:43 +0300

    Add UTs for screen position transformation function
    #3

---
+++ Assets/Tests/UtilityTests.cs
@@ -0,0 +1,41 @@
+﻿using NUnit.Framework;
+using UnityEngine;
+using UnityEngine.TestTools;
+using h8s;
+
+namespace Tests
+{
+    [TestFixture]
+    public class ScreenPositionTransformationTests
+    {
+        private GameObject root;
+
+        static object[] CanvasPositionCases = {
+            // Format:     Screen Size                Screen Position      Expected Canvas Position
+            new object[] { new Vector2(1900f, 1080f), new Vector2(0f, 0f), new Vector2(0f, -1080f) },
+            new object[] { new Vector2(1900f, 1080f), new Vector2(1900f, 1080f), new Vector2(1900f, 0f) },
+            new object[] { new Vector2(1900f, 1080f), new Vector2(500f, 300f), new Vector2(500f, -780f) }
+        };
+
+        [SetUp]
+        public void SetUp()
+        {
+            root = new GameObject();
+            root.AddComponent<Canvas>();
+            root.AddComponent<RectTransform>();
+            root.AddComponent<BlackboardController>();
+        }
+
+        [TestCaseSource("CanvasPositionCases")]
+        public void ScreenPositionIsProperlyTransformedToCanvasPosition(
+            Vector2 canvasSize, Vector2 screenPosition, Vector2 expectedCanvasPosition)
+        {
+            var gui_rect = root.GetComponent<RectTransform>();
+            gui_rect.sizeDelta = canvasSize;
+
+            var computed_canvas_position = Utility.CanvasPositionFromScreen(screenPosition);
+
+            Assert.AreEqual(expectedCanvasPosition, computed_canvas_position);
+        }
+    }
+}
